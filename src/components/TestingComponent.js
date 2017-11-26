455119420e7d79dcbe8953296c0971bb515b467b
Tomas Dulka <dulajz@gmail.com>
2017-11-26 19:30:25 +0100

    First setup

---
+++ src/components/TestingComponent.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class TestingComponent extends Component {
+  render() {
+    return <h1>Hello World</h1>
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestingComponent from './components/TestingComponent';
+
+ReactDOM.render(
+  <TestingComponent />,
+  document.getElementById('root')
+);
