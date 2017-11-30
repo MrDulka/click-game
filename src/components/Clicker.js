d1e0e4f56d4d131763684e0a56afc0dbd2d6eb5f
Tomas Dulka <dulajz@gmail.com>
2017-12-01 00:47:14 +0100

    Make a dumb clicker

---
+++ src/components/Clicker.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Clicker = ({ doClick, sessionClicks, teamClicks }) => {
+  return (
+    <div className = "container">
+      <button className = "btn btn-big" onClick = {doClick}>CLICK!</button>
+      <div className="row">
+        <div className="column">
+          <h3 className = "clicks-label">Your clicks:</h3>
+          <p className = "clicks-score">{sessionClicks}</p>
+        </div>
+        <div className="column">
+          <h3 className = "clicks-label">Team clicks:</h3>
+          <p className = "clicks-score">{teamClicks}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Clicker;
